3d1b5d3b6022eb139ca89c66116579c15fe2d5e6
Naser Al-Asbahi <nasserqahtan0@gmail.com>
2025-05-13 15:04:51 +0200

    Add project files.

---
+++ RazorCms/Models/Page.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace RazorCms.Models
+{
+    public class Page
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Content { get; set; }
+        public bool IsVisible { get; set; }
+        public int OrderIndex { get; set; }
+    }
+}
